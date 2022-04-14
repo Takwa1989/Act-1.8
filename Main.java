d14504aa5dab1beac44289d4e9cba33fcf3ac6c3
Takwa1989 <103288279+Takwa1989@users.noreply.github.com>
2022-04-14 23:03:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 
-public class Main {
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class LecturFile {
+public class Lecteur {
 
 	public static void main(String[] args) {
 		File file= new File("Ma play list.doc");
@@ -16,7 +17,7 @@ public class LecturFile {
 			FileReader fileReader=new FileReader(file);
 			bufferreader = new BufferedReader(fileReader);
 		} catch (FileNotFoundException e) {
-			System.err.printf("le fichier %s inexistante",file.toString());
+			System.err.printf("le fichier %s n'a pas été trouvé",file.toString());
 			
 		}
         try {
@@ -27,4 +28,28 @@ public class LecturFile {
 		}
         } catch(NullPointerException e) {
         	System.err.printf("Impossible d'ouvrir le fichier",file.toString());}
+	}
+	// Fonction enregistrement la playlist d'un utilisateur dans un autre.
+	public void Enregistrement() {
+		try {
+		File file1= new File("Ma play list.doc");
+		File file2= new File("User play list.doc");
+		FileWriter filewriter = new FileWriter("User play list.doc");
+		PrintWriter printWriter= new PrintWriter (filewriter );
+		Scanner scan = new Scanner(file1);
+		while (scan.hasNextLine()) {
+			String texte=scan.nextLine();
+			printWriter.println(texte);}
+		printWriter.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+        
+
+
+        
+	
+	
