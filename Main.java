9f2bc45777b047f47b0550427d16554dd2788271
Takwa1989 <103288279+Takwa1989@users.noreply.github.com>
2022-04-14 22:59:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,30 @@
 
 public class Main {
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LecturFile {
+
+	public static void main(String[] args) {
+		File file= new File("Ma play list.doc");
+		 BufferedReader bufferreader = null;
+        try {
+			FileReader fileReader=new FileReader(file);
+			bufferreader = new BufferedReader(fileReader);
+		} catch (FileNotFoundException e) {
+			System.err.printf("le fichier %s inexistante",file.toString());
+			
+		}
+        try {
+		try {
+			bufferreader.close();
+		} catch (IOException e) {
+			System.err.printf("Impossible de fermer le fichier",file.toString());
+		}
+        } catch(NullPointerException e) {
+        	System.err.printf("Impossible d'ouvrir le fichier",file.toString());}
 }
